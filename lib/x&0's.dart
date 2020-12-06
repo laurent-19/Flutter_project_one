47dd8eb7b392d5ac4c66d788288537e5d221437b
Popa Ioan Laurentiu <ioanlaurentiupopa@yahoo.ro>
2020-12-06 20:34:04 +0200

    Homework_week3_part1

--- lib/x&0's.dart
+++ lib/x&0's.dart
@@ -52,44 +52,28 @@ class MyHomePageState extends State<MyHomePage> {
 
   //check for all possible wining combinations
   bool isGameOver() {
-    if (colors[0] == colors[3] &&
-        colors[3] == colors[6] &&
-        colors[0] != Colors.white) {
+    if (colors[0] == colors[3] && colors[3] == colors[6] && colors[0] != Colors.white) {
       winningCombinations.addAll([0, 3, 6]);
       return true;
-    } else if (colors[1] == colors[4] &&
-        colors[4] == colors[7] &&
-        colors[1] != Colors.white) {
+    } else if (colors[1] == colors[4] && colors[4] == colors[7] && colors[1] != Colors.white) {
       winningCombinations.addAll([1, 4, 7]);
       return true;
-    } else if (colors[2] == colors[5] &&
-        colors[5] == colors[8] &&
-        colors[2] != Colors.white) {
+    } else if (colors[2] == colors[5] && colors[5] == colors[8] && colors[2] != Colors.white) {
       winningCombinations.addAll([2, 5, 8]);
       return true;
-    } else if (colors[0] == colors[1] &&
-        colors[1] == colors[2] &&
-        colors[0] != Colors.white) {
+    } else if (colors[0] == colors[1] && colors[1] == colors[2] && colors[0] != Colors.white) {
       winningCombinations.addAll([0, 1, 2]);
       return true;
-    } else if (colors[3] == colors[4] &&
-        colors[4] == colors[5] &&
-        colors[3] != Colors.white) {
+    } else if (colors[3] == colors[4] && colors[4] == colors[5] && colors[3] != Colors.white) {
       winningCombinations.addAll([3, 4, 5]);
       return true;
-    } else if (colors[6] == colors[7] &&
-        colors[7] == colors[8] &&
-        colors[6] != Colors.white) {
+    } else if (colors[6] == colors[7] && colors[7] == colors[8] && colors[6] != Colors.white) {
       winningCombinations.addAll([6, 7, 8]);
       return true;
-    } else if (colors[0] == colors[4] &&
-        colors[4] == colors[8] &&
-        colors[0] != Colors.white) {
+    } else if (colors[0] == colors[4] && colors[4] == colors[8] && colors[0] != Colors.white) {
       winningCombinations.addAll([0, 4, 8]);
       return true;
-    } else if (colors[2] == colors[4] &&
-        colors[4] == colors[6] &&
-        colors[2] != Colors.white) {
+    } else if (colors[2] == colors[4] && colors[4] == colors[6] && colors[2] != Colors.white) {
       winningCombinations.addAll([2, 4, 6]);
       return true;
     } else
